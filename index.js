57fbe5f0718c75abff1c4b780f9e7aaba007c584
Alişan Karababa <alisankarababa@gmail.com>
2023-09-02 16:20:50 +0300

    mvp ready

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const timerDigits = document.getElementById("timer-digits");
+//const timerDigits = document.getElementById("overall-time-digits");
 
 /*think about the case minutes exceeds 59*/
-/* time is running behind??? */
 
 let timer = {
   centiseconds: 0,
   seconds: 0,
   minutes: 0,
+  lapCentiseconds: 0,
+  lapSeconds: 0,
+  lapMinutes: 0,
   timerDigits: null,
+  lapDigits: null,
   idTimeOut: null,
   timeBase: null,
+  lapTimeBase: null,
+  isLapActive: false,
   init: function () {
-    this.timerDigits = document.getElementById("timer-digits");
+    this.timerDigits = document.getElementById("overall-time-digits");
+    this.lapDigits = document.getElementById("lap-time-digits");
+
+    return this.lapDigits && this.timerDigits;
   },
   start: function () {
     if (!this.idTimeOut) {
@@ -22,6 +30,15 @@ let timer = {
           1000 * this.seconds -
           60000 * this.minutes
       );
+
+      if (this.isLapActive) {
+        this.lapTimeBase = new Date(
+          Date.now() -
+            10 * this.lapCentiseconds -
+            1000 * this.lapSeconds -
+            60000 * this.lapMinutes
+        );
+      }
     }
   },
   run: function () {
@@ -34,6 +51,18 @@ let timer = {
       thisObj.seconds = timeNow.getSeconds();
       thisObj.centiseconds = Math.trunc(timeNow.getMilliseconds() / 10);
       thisObj.setTimerDigits(thisObj.getFormattedTime());
+
+      if (thisObj.isLapActive) {
+        const relativeLapTime = new Date() - thisObj.lapTimeBase;
+        const timeLapNow = new Date(relativeLapTime);
+
+        thisObj.lapMinutes = timeLapNow.getMinutes();
+        thisObj.lapSeconds = timeLapNow.getSeconds();
+        thisObj.lapCentiseconds = Math.trunc(timeLapNow.getMilliseconds() / 10);
+
+        thisObj.setLapDigits(thisObj.getFormattedLapTime());
+      }
+
       thisObj.run();
     }, 200);
   },
@@ -47,8 +76,32 @@ let timer = {
     this.minutes = 0;
     this.centiseconds = 0;
     this.timeBase = null;
+
+    this.lapSeconds = 0;
+    this.lapMinutes = 0;
+    this.lapCentiseconds = 0;
+    this.lapTimeBase = null;
+    this.isLapActive = false;
+
     this.setTimerDigits(this.getFormattedTime());
+    this.setLapDigits(this.getFormattedLapTime());
+
     return this;
+  },
+  lap: function () {
+    this.lapTimeBase = new Date();
+
+    this.lapCentiseconds = 0;
+    this.lapSeconds = 0;
+    this.lapMinutes = 0;
+    this.isLapActive = true;
+    this.setLapDigits(this.getFormattedLapTime());
+    return this;
+  },
+  getFormattedLapTime: function () {
+    return `${this.lapMinutes.toString().padStart(2, "0")}:${this.lapSeconds
+      .toString()
+      .padStart(2, "0")}:${this.lapCentiseconds.toString().padStart(2, "0")}`;
   },
   getFormattedTime: function () {
     return `${this.minutes.toString().padStart(2, "0")}:${this.seconds
@@ -58,11 +111,18 @@ let timer = {
   setTimerDigits: function (formattedDigitStr) {
     this.timerDigits.innerText = formattedDigitStr;
   },
+  setLapDigits: function (formattedDigitStr) {
+    this.lapDigits.innerText = formattedDigitStr;
+  },
 };
 
 const buttons = document.querySelectorAll(".btn");
 const btn1 = document.getElementById("btn-lap");
 const btn2 = document.getElementById("btn-start");
+const secLapRecs = document.getElementById("lap-recs");
+const secLapHeader = document.getElementById("lap-header");
+const secLapRecordsContainer = secLapRecs.firstElementChild;
+let cntLap = 0;
 
 function btnFunc(idBtn) {
   switch (idBtn) {
@@ -70,6 +130,7 @@ function btnFunc(idBtn) {
       btn2.id = "btn-stop";
       btn2.innerText = "Stop";
       timer.start();
+      btn1.disabled = false;
       break;
 
     case "btn-stop":
@@ -84,6 +145,7 @@ function btnFunc(idBtn) {
       btn1.innerText = "Lap";
       btn2.id = "btn-stop";
       btn2.innerText = "Stop";
+
       timer.start();
       break;
     case "btn-reset":
@@ -92,6 +154,39 @@ function btnFunc(idBtn) {
       btn2.id = "btn-start";
       btn2.innerText = "Start";
       timer.reset();
+      btn1.disabled = true;
+      secLapRecordsContainer.replaceChildren();
+      secLapHeader.style.visibility = "hidden";
+      cntLap = 0;
+      break;
+    case "btn-lap":
+      if (!cntLap) {
+        secLapHeader.style.visibility = "visible";
+        timer.lapDigits.style.visibility = "visible";
+      }
+
+      console.log("here");
+
+      const divRow = document.createElement("div");
+      divRow.classList.add("row", "align-items-center");
+      const divColCntLap = document.createElement("div");
+      const divColTimeLap = document.createElement("div");
+      const divColTimeOverall = document.createElement("div");
+      divColCntLap.classList.add("col-sm-12", "col-md-4");
+      divColTimeLap.classList.add("col-sm-12", "col-md-4");
+      divColTimeOverall.classList.add("col-sm-12", "col-md-4");
+
+      divColCntLap.innerText = `${++cntLap}`;
+      divColTimeLap.innerText = timer.getFormattedLapTime();
+      divColTimeOverall.innerText = timer.getFormattedTime();
+
+      divRow.append(divColCntLap);
+      divRow.append(divColTimeLap);
+      divRow.append(divColTimeOverall);
+
+      secLapRecordsContainer.prepend(divRow);
+
+      timer.lap();
       break;
 
     default:
@@ -99,14 +194,19 @@ function btnFunc(idBtn) {
   }
 }
 
-if (!btn1 || !btn2) {
+if (!btn1 || !btn2 || !secLapRecs || !secLapHeader || !secLapRecordsContainer) {
   alert("ERROR!!");
 }
 
 for (const button of buttons) {
   button.addEventListener("click", function () {
-    btnFunc(button.id);
+    //console.log(this); // btn func id's change in btnFunc but why does this print the id as if btnFunc called before this line??
+
+    btnFunc(this.id);
   });
 }
 
-timer.init();
+btn1.disabled = true;
+if (!timer.init()) {
+  alert("ERROR TIMER INIT!");
+}
